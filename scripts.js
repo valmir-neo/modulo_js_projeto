891777cbd96a4754e19fbcda33bd70fb2c4e7f39
valmir-neo <valmirsl@gmail.com>
2025-04-25 04:06:42 -0300

    commit segundo select finalizado

--- scripts.js
+++ scripts.js
@@ -4,7 +4,7 @@ const currencySelect1 = document.querySelector(".currency-select1")
 const currencyValueToConvert = document.querySelector(".currency-value-to-convert")
 
 
-currencySelect1.addEventListener("change", changeCurrency1 )
+currencySelect1.addEventListener("change", changeCurrency1)
 
 
 function changeCurrency1 () {
@@ -42,4 +42,46 @@ function changeCurrency1 () {
         currencyValueToConvert.innerHTML = "R$ 0,00"
     }
 
+}
+
+/*CODIGO P/ MODIFICAR SEGUNDO SELECT*/ 
+const currencySelect2 = document.querySelector(".currency-select2")
+const currencyValueConverted = document.querySelector(".currency-value-converted")
+
+
+currencySelect2.addEventListener("change", changeCurrency2)
+
+function changeCurrency2 () {
+    const currencyName2 = document.querySelector("#currency-name2")
+    const currencyImage2 = document.querySelector(".currency-img2")
+
+    if (currencySelect2.value == "dolar2") {
+        currencyName2.innerHTML = "Dolar Americano"
+        currencyImage2.src = "./assets/dolar.png"
+        currencyValueConverted.innerHTML = "US$ 0,00"
+    }
+
+    if (currencySelect2.value == "euro2") {
+        currencyName2.innerHTML = "Euro"
+        currencyImage2.src = "./assets/euro.png"
+        currencyValueConverted.innerHTML = "0,00 €"
+    }
+
+    if (currencySelect2.value == "libra2") {
+        currencyName2.innerHTML = "Libra Esterlina"
+        currencyImage2.src = "./assets/libra.png"
+        currencyValueConverted.innerHTML = "£ 0,00"
+    }
+
+    if (currencySelect2.value == "bitcoin2") {
+        currencyName2.innerHTML = "Bitcoin"
+        currencyImage2.src = "./assets/bitcoin.png"
+        currencyValueConverted.innerHTML = "₿ 0,00"
+    }
+
+    if (currencySelect2.value == "real2") {
+        currencyName2.innerHTML = "Real"
+        currencyImage2.src = "./assets/real.png"
+        currencyValueConverted.innerHTML = "R$ 0,00"
+    }
 }
